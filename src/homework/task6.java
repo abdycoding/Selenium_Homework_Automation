97335fbc4e48d7b03f1c89a02feeeb94451e03e4
Abylmyrat Gummanov <abdy.coding@gmail.com>
2024-09-05 18:38:50 +0300

    Task 6 is done

--- src/homework/task6.java
+++ src/homework/task6.java
@@ -1,7 +1,10 @@
 package homework;
 
 import Utility.Basedriver;
+import Utility.MyFunc;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class task6 extends Basedriver {
     @Test
@@ -19,6 +22,20 @@ public class task6 extends Basedriver {
         Alert kapanmalıdır.
  */
         driver.get("https://testpages.herokuapp.com/styled/index.html");
+        MyFunc.bekle(2);
+        WebElement fakeAlert= driver.findElement(By.cssSelector("[href='alerts/fake-alert-test.html']"));
+        fakeAlert.click();
+        MyFunc.bekle(2);
+        WebElement show= driver.findElement(By.cssSelector("[value='Show modal dialog']"));
+        show.click();
+        MyFunc.bekle(2);
+
+        WebElement fakeButon= driver.findElement(By.cssSelector("[class='dialog-button']"));
+        fakeButon.click();
+        BekleKapat();
+
+
+
 
     }
 }
